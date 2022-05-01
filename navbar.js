a8c2442d82caa10efac1324748ac4468e73fdf61
MistoMind <mistomind@gmail.com>
2022-05-01 13:12:56 +0530

    First Commit

---
+++ navbar.js
@@ -0,0 +1,48 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+
+ /*===== MENU SHOW =====*/
+ /* Validate if constant exists */
+if(navToggle){
+ navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+ })
+ }
+
+ /*===== MENU HIDDEN =====*/
+ /* Validate if constant exists */
+  if(navClose){
+ navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+  })
+  }
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
