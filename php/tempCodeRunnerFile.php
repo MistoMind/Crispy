2ee3ba9784e1db8dc5b6fee5d7e29d546197d133
anubhuti24 <97036824+anubhuti24@users.noreply.github.com>
2022-06-11 14:21:57 +0530

    remove_test_remove

--- php/tempCodeRunnerFile.php
+++
@@ -1,7 +0,0 @@
-<?php
-while(true){
-			echo $row[$i]['item_name'] . " " . $row[$i]['order_qunatity'] . " " . $row[$i]['price'] . "<br>";
-			$nextID = $row[$i+1][0];
-			if($currID!=$nextID) break;
-			$i++;
-		}
